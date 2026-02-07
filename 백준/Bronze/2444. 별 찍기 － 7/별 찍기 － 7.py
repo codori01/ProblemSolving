09efb7eb593b5ba424864eb8ce56e4ce3609b18b
minseokim <annieminseo0310@gmail.com>
2026-02-08 08:49:34 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,5 @@
+N = int(input())
+for i in range(1,N + 1, 1):
+  print((" " * (N - i)) + ("*" * i) + ("*" * (i - 1)))
+for j in range(N - 1, 0, -1):
+  print((" " * (N - j)) + ("*" * j) + ("*" * (j - 1)))
